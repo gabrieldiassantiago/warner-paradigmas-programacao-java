5f6a5c065fc18ba922952fbb41f88d90c028a07b
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-12 00:46:56 -0300

    Update Salario.java

--- lista/exercicio1/Salario.java
+++ lista/exercicio1/Salario.java
@@ -1,26 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package exerciciolista_1;
+import exerciciolista_1.Salario;
 
-/**
- *
- * @author aluno9
- */
-public class ExercicioLista_1 {
+void main() {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        Salario sal = new Salario();
-        
-        sal.entrada_salario();
-        sal.atualizar_Salario();
-        sal.impressao();    
-    }
-    
+  Scanner scanner = new Scanner(System.in);
+
+  System.out.printf("Digite um valor base:");
+  double valorEntrada = scanner.nextDouble();
+
+  Salario sal = new Salario(valorEntrada);
+
+  sal.calculaAtualizacao(10.0);
+
+  System.out.println("O salario atualizado eh R$: " + sal.getValorAtualizado());
+
 }
