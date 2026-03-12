a842a9a18fe2c679e69e0a6c99fbc0542ca40c6e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-11 21:15:35 -0300

    Create Salario.java

---
+++ lista/exercicio1/Salario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exerciciolista_1;
+
+/**
+ *
+ * @author aluno9
+ */
+public class ExercicioLista_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Salario sal = new Salario();
+        
+        sal.entrada_salario();
+        sal.atualizar_Salario();
+        sal.impressao();    
+    }
+    
+}
