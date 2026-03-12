7246ff4a83869efaa8f41d94387a8318f5e4be2a
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-12 00:47:07 -0300

    Update Salario_class.java

--- lista/exercicio1/Salario_class.java
+++ lista/exercicio1/Salario_class.java
@@ -3,37 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package exerciciolista_1;
-import java.util.Scanner;
 
-/**
- *
- * @author aluno9
- */
 public class Salario {
-    
-    private double salario_antigo;
-    private double salario_novo;
-    private Scanner s;
-    private double aumento;
-    
-    public Salario() {
-        s = new Scanner(System.in);
-        salario_antigo = 0;
-        salario_novo = 0;
-        aumento = 1.25;
+    private double valorBruto;
+    private double valorAtualizado;
+
+    public Salario(double valorBruto) {
+        this.valorBruto = valorBruto;
     }
-    
-    public void entrada_salario() {
-      System.out.println("Informa o seu salario antigo: \n");
-      salario_antigo = s.nextDouble();
+
+    public void calculaAtualizacao(double porcentualAumento) {
+        this.valorAtualizado = this.valorBruto * (1 + (porcentualAumento / 100));
     }
-    
-    public void atualizar_Salario() {
-        salario_novo = salario_antigo * aumento;
-    }
-    
-    public void impressao() {
-        System.out.println("O salário antigo era: \n" + salario_antigo);
-        System.out.println("O novo salário é: " + salario_novo);
+
+    public double getValorAtualizado() {
+        return this.valorAtualizado;
     }
 }
