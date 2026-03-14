a5f07bd929dd4fde424f0c8cf77f5464a692575a
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-14 16:47:54 -0300

    Create Main.java

---
+++ lista/exercicio8/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Aluno aluno = new Aluno();
+
+    aluno.ler_notas();
+    aluno.calcular();
+    aluno.imprimir();
+}
