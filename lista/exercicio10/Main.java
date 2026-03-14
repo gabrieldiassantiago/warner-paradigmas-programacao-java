33822731797ff1c1037884e97305261d5798bd1d
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-14 20:07:22 -0300

    Create Main.java

---
+++ lista/exercicio10/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Primo ex = new Primo();
+
+    ex.ler_numero();
+    ex.verificar_primo();
+}
