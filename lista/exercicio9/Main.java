e1b93ec6fdfddde909d92a976ce3d384f1c3edba
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-16 10:25:08 -0300

    Create Main.java

---
+++ lista/exercicio9/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		Fatorial novoFatorial = new Fatorial();
+		novoFatorial.entrada();
+		novoFatorial.calcular();
+		novoFatorial.imprimir();
+	}
+}
