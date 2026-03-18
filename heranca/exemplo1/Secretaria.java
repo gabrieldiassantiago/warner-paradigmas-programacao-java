8ea9c75daa0753b34b7b2b2001b113c80826149e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-18 20:35:02 -0300

    Create Secretaria.java

---
+++ heranca/exemplo1/Secretaria.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio12;
+
+/**
+ *
+ * @author aluno9
+ */
+public class Secretaria extends Funcionario{
+     
+    private int ramal;
+    
+    
+    public void setRamal(int entraRamal) {
+        this.ramal = entraRamal;
+    }
+    
+    public int getRamal() {
+        return this.ramal;
+    }
+    
+    
+    
+    
+    
+}
