1542ace21ee7f4940fcca9bb34c644a19e17ea3e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-18 20:35:14 -0300

    Create Telefonista.java

---
+++ heranca/exemplo1/Telefonista.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio12;
+
+/**
+ *
+ * @author aluno9
+ */
+public class Telefonista extends Secretaria{
+    private int estacaoTrabalho;
+    
+    public void setEstacao(int entraEstacaoTrabalho) {
+        
+        this.estacaoTrabalho = entraEstacaoTrabalho;
+
+    }
+    
+    public int getEstacaoTrabalho() {
+        return estacaoTrabalho;
+    }
+    
+    
+}
