e3fdce1093a3a69bbaa76d39b8abb0e0395ed1fb
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-18 22:58:37 -0300

    Create Moto.java

---
+++ heranca/exemplo1/Exercicio_lista_1/Moto.java
@@ -0,0 +1,11 @@
+public class Moto extends Transporte {
+
+    public Moto(String marca, String modelo) {
+        super(marca, modelo);
+    }
+
+    public void abaixarDescanso() {
+        System.out.println("Descanso baixado!");
+    }
+
+}
