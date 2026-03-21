5748b50f29ae297d6e964d5a98fafb6c86cc4ee4
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-21 20:21:05 -0300

    Create ClasseEconomica.java

---
+++ heranca/exemplo1/Exercicio_lista_3/ClasseEconomica.java
@@ -0,0 +1,10 @@
+public class ClasseEconomica extends PassagemAerea {
+
+    public ClasseEconomica(double valor) {
+        super(valor);
+    }
+
+    public void imprimeClasse() {
+        System.out.println("Classe Econômica");
+    }
+}
