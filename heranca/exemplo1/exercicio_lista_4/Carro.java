dacd18cb27a1c9d8765e9eb135858fb6b2d12a2f
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-22 00:58:41 -0300

    Create Carro.java

---
+++ heranca/exemplo1/exercicio_lista_4/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    private double kilometragem;
+    private double preco;
+
+    public Carro(double kilometragem, double preco) {
+        this.kilometragem = kilometragem;
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
