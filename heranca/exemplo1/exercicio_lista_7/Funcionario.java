e8ea86710e856fe6b256a8c72d76146ef4d7e75c
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-25 00:26:07 -0300

    Create Funcionario.java

---
+++ heranca/exemplo1/exercicio_lista_7/Funcionario.java
@@ -0,0 +1,34 @@
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private double salarioBase;
+
+    public Funcionario() {
+
+    }
+
+    public void setNome(String entraNome){
+        this.nome= entraNome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setIdade(int entraIdade){
+        this.idade = entraIdade;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setSalarioB(double entraSalario){
+        this.salarioBase = entraSalario;
+    }
+
+    public double getSalarioB(){
+        return this.salarioBase;
+    }
+
+    public double calcular_salario(){
+        return this.salarioBase;
+    }
+}
