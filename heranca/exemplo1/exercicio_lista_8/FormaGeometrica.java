9dbf22f811fb729f9f02654f32c85604764e4035
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-25 00:34:54 -0300

    Create FormaGeometrica.java

---
+++ heranca/exemplo1/exercicio_lista_8/FormaGeometrica.java
@@ -0,0 +1,20 @@
+public class FormaGeometrica {
+    protected double area;
+    protected double perimetro;
+
+    public double calcularArea() {
+        return area;
+    }
+
+    public double calcularPerimetro() {
+        return perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+}
