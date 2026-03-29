e5c5c4b7f77c40fb6a6883e6bd68f107a5a7e2ec
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2026-03-29 17:47:14 -0300

    Create ArrayList.java

---
+++ exercicios_estudo/ArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+void main() {
+ /*
+ Tipo primitivo:
+ int ----> Integer
+ float --> Float
+ double -> Double
+ char  --> Caracter
+
+ArrayList<Tipo> nomeVariavel = new ArrayList<Tipo>();
+ 
+  */
+
+  ArrayList<Integer> valores = new ArrayList<Integer>();
+
+  valores.add(1);
+  valores.add(2);
+
+  for (int m : valores) {
+    System.out.println(m);
+  }
+
+  valores.remove(0); //remove o indice 0
+
+
+
+
+
+}
